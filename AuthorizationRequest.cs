23cc5538b13a52289fdd16d1d275b59134f9f8d4
Javier Sanchez <jfsanchez@agilisa.com>
2022-04-19 16:21:38 -0400

    refactoring de classes

---
+++ AuthorizationRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace TestTransaction
+{
+    internal class AuthorizationRequest
+    {
+        public string MerchantKey { get; set; }
+        public string AccountNumber { get; set; }
+        public string ExpirationMonth { get; set; }
+        public string ExpirationYear { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerId { get; set; }
+        public string AccountType { get; set; }
+        public string CustomerEmail { get; set; }
+        public string ZipCode { get; set; }
+        public string Amount { get; set; }
+        public string Currency { get; set; }
+        public string Tax { get; set; }
+        public string Invoice { get; set; }
+        public string Transaction_Detail { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using RestSharp;
 using System;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TestTransaction
@@ -85,34 +84,6 @@ namespace TestTransaction
             }
             return null;
         }
-
-    }
-
-
-    record TokenResponse
-    {
-        [JsonPropertyName("token_type")]
-        public string TokenType { get; init; }
-        [JsonPropertyName("access_token")]
-        public string AccessToken { get; init; }
-    }
-
-    internal class AuthorizationRequest
-    {
-        public string MerchantKey { get; set; }
-        public string AccountNumber { get; set; }
-        public string ExpirationMonth { get; set; }
-        public string ExpirationYear { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerId { get; set; }
-        public string AccountType { get; set; }
-        public string CustomerEmail { get; set; }
-        public string ZipCode { get; set; }
-        public string Amount { get; set; }
-        public string Currency { get; set; }
-        public string Tax { get; set; }
-        public string Invoice { get; set; }
-        public string Transaction_Detail { get; set; }
 
     }
 }
